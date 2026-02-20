19262e85ad953908c55ea261071fb508d3e89e1b
Vishnu <vishnuspillai04@gmail.com>
2026-02-20 17:16:28 +0530

    feat: add PWA support - installable as Android app

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'neko-timer-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
+];
+
+// Install: cache all assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+// Fetch: serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request).catch(() => caches.match('./index.html'));
+        })
+    );
+});
